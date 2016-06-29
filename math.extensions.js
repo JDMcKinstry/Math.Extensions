61ae6eee50a05816c3da8d4d3423c789593db9ba
JD McKinstry <jdmckinstry@gmail.com>
2016-06-29 12:45:01 -0400

    Update math.extensions.js
    added .rand method

--- math.extensions.js
+++ math.extensions.js
@@ -13,9 +13,10 @@
  *	Math.range()
  *	Math.sum()
  *	Math.runExample()
+ *	Math.rand()
  **/
 ;(function() {
-	/*	[ 'difference', 'max', 'mean', 'median', 'medianMinMax', 'min', 'minMax', 'maxMin', 'mode', 'range', 'sum' ]	*/
+	/*	[ 'average', 'difference', 'max', 'mean', 'median', 'medianMinMax', 'min', 'minMax', 'maxMin', 'mode', 'product', 'quotient', 'range', 'sum' ]	*/
 	var args = Array.prototype.slice.call(arguments, 1),
 		mathMax = Math.max,
 		mathMin = Math.min;
@@ -244,5 +245,15 @@
 		var method = props[name];
 		Object['defineProperty'] && !Math.hasOwnProperty(name)
 			? Object.defineProperty(Math, name, { value: method }) : Math[name] = method;
+	}
+})(Math);
+;(function(){	//	Math.rand	//	Get random number between 2 numbers. 3rd var bool for if to include decimal
+	if (!Math.hasOwnProperty('rand')) {
+		Math.rand = function(min, max, dec) {
+			var r = this.random() * (max - min + 1);
+			if (dec !== true && typeof dec !== "number") return ~~r + ~~min;
+			else if (typeof dec === "number") return r = (r + min).toFixed(dec), r = r <= this.max(max, min) ? r : (r/2).toFixed(dec), (parseFloat(r)+"").length == (r+"").length ? parseFloat(r) : r;
+			return r = r + min, r <= this.max(max, min) ? r : r/2;
+		}
 	}
 })(Math);
